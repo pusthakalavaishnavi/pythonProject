2e864a9054d24f6b30044d3dcb316e07615cf5ad
P VAISHNAVI <vaisnavipusthakala09@gmail.com>
2022-02-10 17:39:39 +0530

    end

--- PythonPractice/practice.py
+++ PythonPractice/practice.py
@@ -25,21 +25,80 @@
 #         for words in i:
 #             d[words] = i.count(d[words])
 #     return d
+#
+# def rec(strng):
+#     print(strng)
+#     return rec(strng[1::])
 
-def rec(strng):
-    print(strng)
-    return rec(strng[1::])
+# var = "python"
+# print(var.count())
+# my_message="hello world"
+# print(my_message.rfind("l"))
+
+"""wap to check even or odd"""
+# var = int(input("enter the num:"))
+# if var%2==0:
+#     print("its even")
+# else:
+#     print("its odd")
+
+"""wap to check given number is lower or not"""
+# var = input("enter the string:")
+# if var.islower():
+#     print("string is lower")
+# else:
+#     print("its not a lower")
+
+"""wap to check given element is present in the list or not"""
+# list = ["python","is","java","xml","html","css"]
+# var = input("enter the string:")
+# if var in list:
+#     print("true")
+# else:
+#     print("false")
+#
+#
+
+"""wap to check given char is empty or not"""
+s = "hI"
+# if len(s)>0:
+#     print("not empty")
+# else:
+#     print("empty")
+
+# if s:
+#     print("not empty")
+# else:
+#     print("empty")
+
+"""wap to find default or notdefault"""
+# s=[]
+# if s:
+#     print("it is not default")
+# else:
+#     print("default")
+
+"""wap to convert lower to uper case and upper case to lower"""
+# var = input("enter the string:")
+# # print(var.swapcase())
+# if "a"<=var<="z":
+#     print(chr(ord(var)-32))
+# else:
+#     if "A"<=var<="Z":
+#         print(chr(ord)
 
 
 
+# def fun_(num):
+#     if num>0:
+#         print(11)
+#         return fun_(num-1)
+#
+# fun_(11)
 
-
-
-
-
-
-
-
+l=[10,20,30,40]
+for i in range(len(l)):
+    print(l[i]**i)
 
 
 
